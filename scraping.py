76e3ae1bebd1a6b2bcccfc25bef424054ef03e00
husita-h <taisei.fukushi1129@gmail.com>
2022-08-08 01:08:11 +0900

    github actionsのjob名を変更

--- scraping.py
+++ scraping.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup # http://kondou.com/BS4/
 import pdb # pdb.set_trace()
-# from git import Repo # https://gitpython.readthedocs.io/en/stable/tutorial.html
 
 URL = 'https://www.city.kita.tokyo.jp/d-shisetu/kurashi/bus/bus.html'
 
@@ -20,12 +19,11 @@ def compare_element() -> bool:
             old_data = f.read()
             if old_data == new_data:
                 print("Page is not updated")
-                # commit_diff()
                 return False
             else:
                 print("Page is updated!!")
                 over_write_current_element(new_data)
-                # commit_diff()
+                # ページの更新があったときに通知したい
                 return True
     except:
         print("Except is occured")
@@ -36,9 +34,6 @@ def over_write_current_element(new_data: str) -> bool:
     with open('tmp/old_data.txt', "w") as f:
         f.write(new_data)
         return True
-
-# def commit_diff():
-#     repo = git.Repo()
 
 if __name__ == "__main__": 
     compare_element()
