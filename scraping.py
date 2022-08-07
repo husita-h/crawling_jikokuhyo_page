2f1b06550092050959469f8be99646b6aab7c3fa
husita-h <taisei.fukushi1129@gmail.com>
2022-08-08 00:38:26 +0900

    git-auto-commit-actionを追加
    https://github.com/stefanzweifel/git-auto-commit-action

--- scraping.py
+++ scraping.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup # http://kondou.com/BS4/
 import pdb # pdb.set_trace()
+# from git import Repo # https://gitpython.readthedocs.io/en/stable/tutorial.html
 
 URL = 'https://www.city.kita.tokyo.jp/d-shisetu/kurashi/bus/bus.html'
 
@@ -15,23 +16,29 @@ def get_current_element() -> str:
 def compare_element() -> bool:
     new_data = get_current_element()
     try:
-        with open('tmp/old_element.txt', "r") as f:
+        with open('tmp/old_data.txt', "r") as f:
             old_data = f.read()
             if old_data == new_data:
-                print("page is not updated")
+                print("Page is not updated")
+                # commit_diff()
                 return False
             else:
-                print("page is updated!!")
+                print("Page is updated!!")
                 over_write_current_element(new_data)
+                # commit_diff()
                 return True
     except:
-        print("except is occured")
+        print("Except is occured")
+        return False
         # エラーのときに通知したい
 
 def over_write_current_element(new_data: str) -> bool:
-    with open('tmp/old_element.txt', "w") as f:
+    with open('tmp/old_data.txt', "w") as f:
         f.write(new_data)
         return True
+
+# def commit_diff():
+#     repo = git.Repo()
 
 if __name__ == "__main__": 
     compare_element()
